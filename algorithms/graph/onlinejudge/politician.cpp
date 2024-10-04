b7613a5ba7330513a7ca07e8fe4e40917a74d51e
Sergey Yakovlev <fclaw007@gmail.com>
2024-10-04 14:51:26 +0300

    1

---
+++ algorithms/graph/onlinejudge/politician.cpp
@@ -0,0 +1,14 @@
+
+
+namespace algorithms::graph::onlinejudge::politician
+{
+
+#define loop(x, s, n) for(int x = s; x < n; x++)
+#define back_loop(x, s, n) for(int x = s; x >= n; x--)
+
+
+    /** https://onlinejudge.org/external/105/10543.pdf , 
+     * topic: Converting General Graph to DAG
+     * state: pos, given speech */
+    int determineMinSpeeches() { return 1; }
+}
--- algorithms/graph/onlinejudge/winterim.cpp
+++ algorithms/graph/onlinejudge/winterim.cpp
@@ -3,12 +3,30 @@
 #include <optional>
 #include <iostream>
 #include <unordered_map>
+#include <string>
+#include <utility>
 
 namespace algorithms::graph::onlinejudge::winterim
 {
 
-typedef std::vector<int> vi;
 
+struct pair_hash 
+{
+    std::size_t operator () (const std::pair<int, int> &p) const 
+    {
+        auto first = std::hash<int>{}(p.first);
+        auto second = std::hash<int>{}(p.second);
+        // Mainly for demonstration purposes, i.e. works but is overly simple
+        // In the real world, use sth. like boost.hash_combine
+       return first ^ (second + 0x9e3779b9 + (first << 6) + (first >> 2));
+    }
+};
+
+// x - counter. s - start point, n - boundary 
+#define loop(x, s, n) for(int x = s; x < n; x++)
+typedef std::vector<int> vi;
+typedef std::pair<int, int> state;
+typedef std::unordered_map<state, int, pair_hash> table; 
 
     /** https://onlinejudge.org/external/9/907.pdf. 
      * closely resemble  https://leetcode.com/problems/find-minimum-time-to-finish-all-jobs
@@ -22,34 +40,31 @@ typedef std::vector<int> vi;
      * the cost (maximum amount of walking done in one day) is 10.
      * Another schedule that involves walking 9, 6, 9 miles on each day has cost 9.
      * hints (Converting General Graph to DAG): state(pos, night left)
-     * an implicit dag (tree) of partial solutions
-     * */
-    void backtrack(const vi& camps, int p, int nights, int local_max, int& ans) 
+     * an implicit dag (tree) of partial solutions */
+    int backtrack(const vi& camps, int p, int nights, table& memo)
     {
         int S = camps.size();
         if(p < S && nights < 0)
-          return;
+          return INT32_MAX;
 
         if(p == S && nights < 0)
-        {
-            ans = std::min(ans, local_max);
-            return;
-        }
+            return 0;
+
+        if(auto it = memo.find({p, nights}); 
+           it != memo.end())
+          return memo[{p, nights}];   
 
         int acc = 0;
-        for(int i = p; i < S; i++)
-        {
-            acc += camps[i];
-            backtrack(camps, i + 1, nights - 1, std::max(local_max, acc), ans);
-        }
+        int ans = INT32_MAX;
+        loop(i, p, S)
+          acc += camps[i],
+          ans = std::min(ans, std::max(acc, backtrack(camps, i + 1, nights - 1, memo)));
+        return memo[{p, nights}] = ans;
     }
 
-    int deviseCampingStrategy(const vi& camps, int nights) 
-    {
-        int ans = INT32_MAX;
-        backtrack(camps, 0, nights, 0, ans);
-        return ans;
-    }
+    int deviseCampingStrategy(const vi& camps, int nights)
+    { table memo; return backtrack(camps, 0, nights, memo); }
+
     void submit(std::optional<char*> file)
     {
         if(file.has_value()) 

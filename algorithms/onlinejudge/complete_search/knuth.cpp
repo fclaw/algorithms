ab5020780d77e77a0303fcf67bcc0c2750334c20
Sergey Yakovlev <fclaw007@gmail.com>
2025-03-09 18:09:53 +0300

    improvement

--- algorithms/onlinejudge/complete_search/knuth.cpp
+++ algorithms/onlinejudge/complete_search/knuth.cpp
@@ -17,33 +17,25 @@ typedef std::vector<std::string> vs;
 namespace algorithms::onlinejudge::complete_search::knuth
 {
     /** https://onlinejudge.org/external/100/10063.pdf */
-    void backtrack(const vc& letters, int i, vs tmp, vs& ans)
+    void backtrack(const vc& letters, int i, vs xs, vs& ans)
     {
         if(i == letters.size()) 
-        { ans = tmp; return; }
+        { ans = xs; return; }
 
-        vs new_tmp;
-        for(const auto& s : tmp)
+        vs new_xs;
+        if(xs.empty())
+          new_xs.push_back(std::string(1, letters[i])); // Start with first letter
+        else 
         {
-            // add at front
-            std::string new_s_front = s;
-            new_s_front.insert(0, 1, letters[i]); 
-            new_tmp.push_back(new_s_front);
-
-          for(int j = 1; j < s.size(); j++)
-          {
-              std::string new_s = s; 
-              new_s.insert(j, 1, letters[i]);
-              new_tmp.push_back(new_s);
-          }
-
-            // add at back
-            std::string new_s_back = s;
-            new_s_back.push_back(letters[i]);
-            new_tmp.push_back(new_s_back);  
-          }
-
-        backtrack(letters, i + 1, new_tmp, ans);
+            for(const auto& x : xs)
+              for(int k = 0; k <= x.size(); k++) 
+              {
+                  std::string left = x.substr(0, k);
+                  std::string right = x.substr(k, x.size() - k); // Fixing substring length
+                  new_xs.push_back(left + letters[i] + right);
+              }
+        }
+        backtrack(letters, i + 1, new_xs, ans);
 
     }
     void submit(std::optional<char*> file)
@@ -61,7 +53,7 @@ namespace algorithms::onlinejudge::complete_search::knuth
             for(auto c : l) 
               letters.push_back(c);
             vs ans;
-            backtrack(letters, 1, {std::string(1, letters.front())}, ans);
+            backtrack(letters, 0, {}, ans);
             for(auto a : ans)
               std::cout << a << std::endl;
         } 

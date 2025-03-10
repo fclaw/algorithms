e62f8717462fd712898e0a45b42b09f4c7b242c1
Sergey Yakovlev <fclaw007@gmail.com>
2025-03-10 20:00:51 +0300

    leetcode: https://leetcode.com/problems/find-minimum-time-to-finish-all-jobs/

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
-#include "algorithms/onlinejudge/complete_search/dna.cpp"
-
-
+// #include "algorithms/onlinejudge/complete_search/dna.cpp"
+#include "algorithms/backtrack/leetcode/jobs.cpp"
+#include <iostream>
 
 int main(int argc, char* argv[])
 {
@@ -9,8 +9,9 @@ int main(int argc, char* argv[])
     std::cout << std::endl;
     std::cout << "--- ... ---\n";
     
-    std::optional<char*> file = std::nullopt;
-    if(argc > 1) file = std::make_optional(argv[1]);
+    // std::optional<char*> file = std::nullopt;
+    // if(argc > 1) file = std::make_optional(argv[1]);
 
-    algorithms::onlinejudge::complete_search::dna::submit(file);
+    // algorithms::onlinejudge::complete_search::dna::submit(file);
+    std::cout << algorithms::backtrack::leetcode::jobs::minimumTimeRequired({1,2,4,7,8}, 2);
 }

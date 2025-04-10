75fdb11c96d9bdfb9a98c78037a2611acf9e31ed
Sergey Yakovlev <fclaw007@gmail.com>
2025-04-10 13:39:54 +0300

    online judge: https://onlinejudge.org/external/105/10502.pdf

--- algorithms/onlinejudge/complete_search/rectangles.cpp
+++ algorithms/onlinejudge/complete_search/rectangles.cpp
@@ -14,12 +14,31 @@ typedef std::vector<vi> vvi;
 namespace algorithms::onlinejudge::complete_search::rectangles
 {
     /** https://onlinejudge.org/external/105/10502.pdf, 6 nested loops, rectangle, not too hard  */
+    int M, N;
+    int getCnt(const vvi& board) 
+    { 
+        int counter = 0;
+        for(int r = 0; r < M; r++)
+          for(int c = 0; c < N; c++)
+            if(board[r][c])
+            {
+                for(int l = 1; l <= N - c; l++)
+                  for(int h = 1; h <= M - r; h++)
+                  {
+                      bool isFailed = false;
+                      for(int i = r; i < r + h && !isFailed; i++)
+                        for(int j = c; j < c + l && !isFailed; j++)
+                          if(!board[i][j]) isFailed = true;
+                     if(!isFailed) counter++;
+                  }
+            }
+        return counter;    
+    }
     void submit(std::optional<char*> file)
     {
         if(file.has_value())
           assert(std::freopen(file.value(), "r", stdin) != nullptr);
         
-        int M, N;
         while(std::cin >> M && M)
         {
             std::cin >> N;
@@ -31,14 +50,7 @@ namespace algorithms::onlinejudge::complete_search::rectangles
                   std::cin >> c;
                   board[i][j] = (int)c - 48;
               }
-            
-            int cnt = 0;
-            for(int i = 0; i < M; i++)
-              for(int j = 0; j < N; j++)
-                if(board[i][j])
-                {}
-                
-            std::cout << cnt << std::endl;      
+            std::cout << getCnt(board) << std::endl;   
         }
     }
 }

62ec83dead8a427644b387f8b183180b61446bba
Sergey Yakovlev <fclaw007@gmail.com>
2025-04-19 10:12:50 +0300

    nix: 24.11

--- shell.nix
+++ shell.nix
@@ -1,9 +1,10 @@
 let
-   pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-24.05.tar.gz") {};
+   pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-24.11.tar.gz") {};
    clang = pkgs.gcc;
    py = pkgs.python3;
+   git = pkgs.git; 
 in
 pkgs.mkShell {
-  buildInputs = [ clang pkgs.boost py (py.withPackages (ps: [ps.numpy])) ];
+  buildInputs = [ git clang pkgs.boost py (py.withPackages (ps: [ps.numpy])) ];
   NIX_PATH = "nixpkgs=" + pkgs.path;
   }

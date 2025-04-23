6f88feeb338e01a4ab7e73b297abac59f6c98d39
Sergey Yakovlev <fclaw007@gmail.com>
2025-04-23 08:29:50 +0300

    https://onlinejudge.org/external/104/10474.pdf, 679 Dropping Balls

---
+++ algorithms/onlinejudge/divide_conquer/balls.cpp
@@ -0,0 +1,47 @@
+#include "../debug.h"
+#include <cassert>
+#include <optional>
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+
+
+
+
+
+typedef std::vector<int> vi;
+
+namespace algorithms::onlinejudge::divide_conquer::balls
+{
+    /** https://onlinejudge.org/external/104/10474.pdf, simple: use sort and then lower bound  */
+    void submit(std::optional<char*> file, bool debug_mode)
+    {
+        if(file.has_value())
+          assert(std::freopen(file.value(), "r", stdin) != nullptr);
+        
+        int tc;
+        std::cin >> tc;
+        while(tc--)
+        {
+            int depth, drop;
+            std::cin >> depth >> drop;
+
+            int it = 1, d = 1;
+            --drop;
+            while(drop)
+            {
+                if(!(drop % 2)) it *= 2;
+                else it = 2 * it + 1;
+                drop /= 2;
+                d++;
+            }
+
+            while(d++ != depth) it *= 2;
+            std::cout << it << std::endl;
+        }
+        int garbage;
+        std::cin >> garbage;
+    }
+}

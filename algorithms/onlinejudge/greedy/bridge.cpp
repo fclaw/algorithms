848b2ae60557f8ea2a7abab253f0f9323f0f4e4c
Sergey Yakovlev <fclaw007@gmail.com>
2025-04-30 08:34:19 +0300

    gitgnore

---
+++ algorithms/onlinejudge/greedy/bridge.cpp
@@ -0,0 +1,40 @@
+#include "../debug.h"
+#include <optional>
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <stdexcept>
+#include <cstring>
+#include <tuple>
+
+
+
+
+
+typedef std::vector<int> vi;
+
+
+namespace algorithms::onlinejudge::greedy::bridge
+{
+    /** https://onlinejudge.org/external/100/10037.pdf */
+    void submit(std::optional<char*> file, bool debug_mode)
+    {
+        if (file.has_value())
+          // Attempt to reopen stdin with the provided file
+          if (std::freopen(file.value(), "r", stdin) == nullptr) {
+            // If freopen fails, throw an exception with a more detailed error message
+            std::string name = file.value();
+            std::string errorMessage = 
+              "Failed to open file: " + name +
+              " with error: " + std::strerror(errno);
+            throw std::ios_base::failure(errorMessage);
+          }
+
+        int tc;
+        std::cin >> tc;
+        std::cin.ignore();
+        while(tc--);
+    }
+}

f75f6b0bee3cd14f00d50ecb55a26d43ac9a5f9e
Sergey Yakovlev <fclaw007@gmail.com>
2025-05-11 10:06:27 +0300

    fix prev

--- algorithms/onlinejudge/greedy/dynamicFrog.cpp
+++ algorithms/onlinejudge/greedy/dynamicFrog.cpp
@@ -22,45 +22,42 @@
 struct Boulder
 {
     uint32_t d;
-    bool is_one_time_usage;
+    bool is_one_time;
 };
 
 
 std::ostream& operator << 
   (std::ostream& out, const Boulder& b) 
-  { return out << "{" << b.d << ", "  << b.is_one_time_usage << "}"; }
+  { return out << "{" << b.d << ", "  << b.is_one_time << "}"; }
 
 typedef std::vector<Boulder> vb;
-typedef std::vector<uint32_t> vi;
-typedef std::unordered_map<int, std::unordered_map<uint64_t, uint32_t>> mimii;
+typedef std::vector<int> vi;
 
 
 
 namespace algorithms::onlinejudge::greedy::dynamic_frog
 {
     /** https://onlinejudge.org/external/111/11157.pdf */
-    uint32_t backtrack(const vb& boulders, int i, vi& jumps, uint64_t used, mimii& memo)
+    int tc, c = 1;
+    uint32_t n, d;
+    uint32_t backtrack(const vb& boulders, int i, uint32_t jump, vi& memo)
     {
         int s = boulders.size();
-        if(i == s)
-        { return *std::max_element(jumps.begin(), jumps.end()); }
+        if(i == s - 1)
+        { return jump; }
 
-        if(memo.count(i) && 
-           memo[i].count(used)) 
-          return memo[i][used];
+        if(~memo[i]) 
+          return memo[i];
 
         uint32_t min_jump = UINT32_MAX;
-        for(int j = i; j < s; j++) {
-          uint32_t jump = boulders[j].d - boulders[i - 1].d;
-          jumps.push_back(jump);
-          if(boulders[j].is_one_time_usage && 
-             !(used & (1LL << (s - j - 1))))
-            min_jump = std::min(min_jump, backtrack(boulders, j + 1, jumps, used | (1LL << j), memo));
-          else if(!boulders[j].is_one_time_usage)
-            min_jump = std::min(min_jump, backtrack(boulders, j + 1, jumps, used, memo));
-          jumps.pop_back();
-        } 
-        return memo[i][used] = min_jump;
+        for(int j = i + 1; j < s; ++j) {
+          uint32_t curr_jump = boulders[j].d - boulders[i].d;
+          if(boulders[j].is_one_time && j + 1 < s)
+            jump = std::max(jump, boulders[j + 1].d - boulders[i].d);
+          else jump = std::max(jump, curr_jump);
+          min_jump = std::min(min_jump, backtrack(boulders, j, jump, memo));
+        }
+        return memo[i] = min_jump;
     }
     void submit(std::optional<char*> file, bool debug_mode)
     {
@@ -75,28 +72,24 @@ namespace algorithms::onlinejudge::greedy::dynamic_frog
             throw std::ios_base::failure(errorMessage);
           }
 
-        int tc, c = 1;
         std::cin >> tc;
-        while(tc--)
+        while(tc-- && while_read(n, d))
         {
-            uint32_t n, d;
-            while_read(n, d);
-            uint32_t s = 2 * n + 3;
+            uint32_t s = n + 2;
             vb boulders(s);
             std::string token;
             boulders[n + 1] = {d, 0};
-            boulders[s - 1] = {2 * d, 0};
+            // boulders[s - 1] = {2 * d, 0};
             for(uint32_t i = 1; i <= n; i++) {
               std::cin >> token;
               char type = token[0]; // 'B' or 'S'
               uint32_t bd = std::stoi(token.substr(2)); // Get the number after '-'
               boulders[i] = {bd, type == 'S'};
-              boulders[s - i - 1] = {2 * d - bd, type == 'S'};
             }
 
-            vi jumps;
-            mimii memo;
-            printf("Case %d: %u\n", c++, backtrack(boulders, 1, jumps, 0, memo));
+            // dbg(boulders);
+            vi memo(101, -1);
+            printf("Case %d: %u\n", c++, backtrack(boulders, 0, 0, memo));
         }
     }
 }

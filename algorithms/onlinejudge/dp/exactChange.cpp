cb4dc2236e0a6e3f6e699c02178fcf2427bf5b6f
Sergey Yakovlev <fclaw007@gmail.com>
2025-05-28 14:56:40 +0300

    online judge: https://onlinejudge.org/external/115/11517.pdf, 11517	Exact Change, still TLE

--- algorithms/onlinejudge/dp/exactChange.cpp
+++ algorithms/onlinejudge/dp/exactChange.cpp
@@ -18,7 +18,7 @@
 #include <bitset>
 #include <unordered_map>
 #include <unordered_set>
-
+#include <tuple>
 
 
 
@@ -26,15 +26,13 @@
 
 
 typedef std::vector<int> vi;
-typedef std::pair<int, vi> pivi;
-typedef std::vector<pivi> vp;
-typedef std::vector<vp> vvp;
+typedef std::tuple<int, int, int> pivi;
 
 bool better(const pivi& a, const pivi& b) 
 {
-    if(a.first != b.first)
-      return a.first < b.first;
-    return a.second.size() < b.second.size();
+    if(std::get<0>(a) != std::get<0>(b))
+      return std::get<0>(a) < std::get<0>(b);
+    return std::get<2>(a) < std::get<2>(b);
 }
 
 namespace algorithms::onlinejudge::dp::exact_change
@@ -42,19 +40,20 @@ namespace algorithms::onlinejudge::dp::exact_change
     /** https://onlinejudge.org/external/115/11517.pdf */
     int tc, price, n;
     const int max = (int)1e5;
-    pivi def = {max, {}};
+    pivi def = {max, 0, 0};
     pivi memo[101][10001];
-    pivi coin_change(const vi& coins, int i, int sum) 
+    pivi coin_change(const vi& coins, int i, int sum)
     {
         if(i == n && sum > 0) return def;
-        if(sum <= 0) return {price - sum, {}};
+        if(sum <= 0) return {price - sum, 0, 0};
          
         pivi& best = memo[i][sum];
         if(best != def) return best;
 
         pivi skip = coin_change(coins, i + 1, sum);
         pivi add = coin_change(coins, i + 1, sum - coins[i]);
-        add.second.push_back(i);
+        std::get<1>(add) += coins[i];
+        std::get<2>(add) += 1;
         return best = better(skip, add) ? skip : add;
     }
     void submit(std::optional<char*> file, bool debug_mode)
@@ -83,10 +82,8 @@ namespace algorithms::onlinejudge::dp::exact_change
             for(int i = 0; i <= 100; ++i)
               for(int j = 0; j <= 10000; ++j)
                 memo[i][j] = def; // or any other sentinel values
-            vi xs = coin_change(coins, 0, price).second;
-            int sum = 0;
-            for(int x : xs) sum += coins[x];
-            std::cout << sum << " " << xs.size() << std::endl;
+            pivi ans = coin_change(coins, 0, price);
+            std::cout << std::get<1>(ans) << " " << std::get<2>(ans) << std::endl;
         }  
     }
 }

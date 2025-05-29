b40a1db5e6b0e9918e7cc5711ff38acf9fea16f5
Sergey Yakovlev <fclaw007@gmail.com>
2025-05-29 12:40:42 +0300

    online judge: https://onlinejudge.org/external/115/11517.pdf, 11517 ExactChange, fix

--- algorithms/onlinejudge/dp/exactChange.cpp
+++ algorithms/onlinejudge/dp/exactChange.cpp
@@ -33,6 +33,7 @@ struct Ans
 bool operator < (const Ans& lhs, const Ans& rhs) 
 { return lhs.diff < rhs.diff || (lhs.diff == rhs.diff && lhs.counter < rhs.counter); }
 
+
 bool operator != (const Ans& lhs, const Ans& rhs) 
 { return !(lhs.diff == rhs.diff && lhs.counter == rhs.counter && lhs.sum == rhs.sum); }
 
@@ -42,27 +43,33 @@ typedef std::vector<Ans> v_ans;
 typedef std::vector<v_ans> vv_ans;
 
 
+const int max = (int)1e5;
+Ans def = {max, 0, 0};
+
 namespace algorithms::onlinejudge::dp::exact_change
 {
     /** https://onlinejudge.org/external/115/11517.pdf */
     int tc, price, n;
-    const int max = (int)1e5;
-    Ans def = {max, 0, 0};
     vv_ans memo;
+    bool is_exact = false;
     Ans coin_change(const vi& coins, int i, int sum)
     {
-        if(i == n && sum > 0) return def;
-        if(sum <= 0) return {price - sum, 0, 0};
+        if(i == n && sum < price) return def;
+        if(sum >= price) {
+            Ans res = def;
+            res.diff = sum - price;
+            return res;
+        }
          
         Ans& best = memo[i][sum];
         if(best != def) return best;
 
         best = def;
         for(int j = i; j < n; ++j) {
-          Ans local_ans = coin_change(coins, j + 1, sum - coins[j]);
+          Ans local_ans = coin_change(coins, j + 1, sum + coins[j]);
           local_ans.counter += 1;
           local_ans.sum += coins[j];
-          if(local_ans < best) best = local_ans;
+          best = std::min(best, local_ans);
         }
         return best;
     }
@@ -85,14 +92,12 @@ namespace algorithms::onlinejudge::dp::exact_change
             while_read(price);
             while_read(n);
             vi coins(n);
-            loop(n, [&coins] (int i) {
-              while_read(coins[i]);
-            });
+            loop(n, [&coins] (int i) {while_read(coins[i]);});
 
-            std::sort(coins.begin(), coins.end(), std::greater<int>());
+            int max_sum = std::accumulate(coins.begin(), coins.end(), 0);
             memo.clear();
-            memo = vv_ans(n + 1, v_ans(price + 1, def));
-            Ans ans = coin_change(coins, 0, price);
+            memo = vv_ans(n + 1, v_ans(max_sum + 1, def));
+            Ans ans = coin_change(coins, 0, 0);
             std::cout << ans.sum << " " << ans.counter << std::endl;
         }  
     }

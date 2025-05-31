c0c7196c4230af6c8d33ad2a31049d4bf0c434bb
Sergey Yakovlev <fclaw007@gmail.com>
2025-05-31 21:34:31 +0300

    online judge: https://onlinejudge.org/external/112/11284.pdf, 11284 Shopping Trip

--- algorithms/onlinejudge/dp/shoppingTrip.cpp
+++ algorithms/onlinejudge/dp/shoppingTrip.cpp
@@ -23,41 +23,44 @@
 
 
 
+struct Shop
+{
+    int id;
+    int discount;
+};
+
+std::ostream& operator << 
+  (std::ostream& out, const Shop& shop) 
+  { return out << "{" << shop.id << ", " << shop.discount << "}"; }
+
 
 typedef std::vector<int> vi;
 typedef std::vector<vi> vvi;
+typedef std::vector<Shop> v_shop;
 
 
 namespace algorithms::onlinejudge::dp::shopping_trip
 {
     /** https://onlinejudge.org/external/112/11284.pdf */
-    int v, e, n, tc;
+    int v, e, n, tc, k;
     vvi fw_sp;
-    vi disks;
+    v_shop shops;
     const int MAX = (int)1e5;
-    // int memo[1 << ]
-    int tcp(int prev, int visited, int expense, vvi& memo)
+    int tcp(int prev, int visited, vvi& memo)
     {
-        if(expense < 0) return INT32_MIN;
-        if(__builtin_popcountll(visited) == v + 1) {
-          int cost = expense - fw_sp[prev][0];
-          if(cost < 0) cost = INT32_MIN;
-          return cost;
-        }
+        if(__builtin_popcount(visited) == k)
+          return -fw_sp[shops[prev].id][0];
 
-        int& best = memo[prev][visited];
-        if(~best) return best;
+       int& best = memo[prev][visited];
+       if(~best) return best;
 
-        best = INT32_MIN;
-        int go_home = expense - fw_sp[prev][0]; // go home
-        if(go_home > 0) best = std::max(best, go_home);
-        for(int i = 1; i <= v; ++i)
+        best = -fw_sp[shops[prev].id][0]; // go home
+        for(int i = 0; i < k; ++i)
           if(!(visited & (1 << i))) {
-            int new_exp = expense - fw_sp[prev][i] + disks[i];
-            int go_ahead = tcp(i, visited | (1 << i), new_exp, memo);
+            int go_ahead = shops[i].discount - fw_sp[shops[prev].id][shops[i].id] + tcp(i, visited | (1 << i), memo);
             best = std::max(best, go_ahead);
           }
-        return best;
+        return  best;
     }
     void submit(std::optional<char*> file, bool debug_mode)
     {
@@ -75,7 +78,7 @@ namespace algorithms::onlinejudge::dp::shopping_trip
         while_read(tc);
         while(tc--)
         {
-            disks.clear();
+            shops.clear();
             fw_sp.clear();
             while_read(v, e);
             fw_sp.resize(v + 1, vi(v + 1, MAX));
@@ -90,26 +93,30 @@ namespace algorithms::onlinejudge::dp::shopping_trip
             });
 
             while_read(n);
-            disks.resize(v + 1);
-            loop(n, [](int _) {
-              int i;
+            std::unordered_map<int, int> tmp;
+            loop(n, [&tmp](int i) {
+              int id;
               double p;
-              while_read(i, p);
-              disks[i] += (int)std::round(p * 100);
+              while_read(id, p);
+              tmp[id] += (int)std::round(p * 100);
             });
 
-            // Floyd Warshall’s
+            for(auto p : tmp) 
+              shops.push_back({p.first, p.second});
+            k = (int)shops.size();
+
+            // Floyd Warshall's
             for(int k = 0; k <= v; ++k)
               for(int i = 0; i <= v; ++i)
                 for(int j = 0; j <= v; ++j)
                   if(i != j) fw_sp[i][j] = std::min(fw_sp[i][j], fw_sp[i][k] + fw_sp[k][j]);    
-    
+
             int max_saving = INT32_MIN;
-            for(int i = 1; i <= v; ++i) {
-              vvi memo(v + 1, vi(1 << (n + 1), -1));
-              max_saving = std::max(max_saving, tcp(i, 0 | (1 << i), disks[i] - fw_sp[0][i], memo));
+            for(int i = 0; i < k; ++i) {
+              vvi memo(k + 1, vi(1 << (k + 1), -1));
+              max_saving = std::max(max_saving, shops[i].discount - fw_sp[0][shops[i].id] + tcp(i, 0 | (1 << i), memo));
             }
-            max_saving != INT32_MIN ? printf("Daniel can save $%0.2f\n", (double)max_saving / 100) : printf("Don't leave the house\n");
+            max_saving > 0 ? printf("Daniel can save $%0.2f\n", (double)max_saving / 100) : printf("Don't leave the house\n");
         }
     }
 }

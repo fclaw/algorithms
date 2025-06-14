cf5a869f8e45c2ced91197f18c1115fcd9de2e8b
Sergey Yakovlev <fclaw007@gmail.com>
2025-06-14 20:56:15 +0300

    online judge:  https://onlinejudge.org/external/100/10029.pdf, 10029 Edit Step Ladders, unfinished

--- algorithms/onlinejudge/graph/esl.cpp
+++ algorithms/onlinejudge/graph/esl.cpp
@@ -6,7 +6,7 @@
 
 #include "../debug.h"
 #include "../../aux.h"
-#include "tools/dfs.cpp"
+#include "tools/bfs.cpp"
 
 #include <optional>
 #include <iostream>
@@ -19,16 +19,60 @@
 #include <bitset>
 #include <unordered_map>
 #include <unordered_set>
+#include <set>
+#include <map>
 
 
 
 namespace tools = algorithms::onlinejudge::graph::tools;
 
 
-namespace algorithms::onlinejudge::graph::battleships
+namespace algorithms::onlinejudge::graph::esl
 {
     /** https://onlinejudge.org/external/100/10029.pdf  */
-    int t_cases, S;
+    void backtrack(
+      const std::map<std::string, int>& word_node,
+      const std::string& w, 
+      tools::vv_def_node& graph,
+      std::set<std::string>& used) {
+
+      int node = word_node.at(w);
+      for(int i = 0; i < (int)w.size(); ++i) {
+        auto tmp = w;
+        // delete char
+        tmp.erase(i, 1);
+        if(word_node.count(tmp) && 
+           used.insert(tmp).second) {
+          int n = word_node.at(tmp);
+          graph[node].push_back(tools::mkDefNode(n));
+          backtrack(word_node, tmp, graph, used);
+        }
+        // replace by next
+        for (char c = 'a'; c <= 'z'; ++c) {
+          if(c == w[i]) continue; // skip same character
+          tmp = w;
+          tmp[i] = c;
+          if(word_node.count(tmp) && 
+             used.insert(tmp).second) {  
+            int n = word_node.at(tmp);
+            graph[node].push_back(tools::mkDefNode(n));
+            backtrack(word_node, tmp, graph, used);
+          }
+        }
+        // add
+        tmp = w;
+        tmp.push_back(' ');
+        for(char c = 'a'; c <= 'z'; ++c) {
+          tmp[tmp.size() - 1] = c;
+          if(word_node.count(tmp) && 
+             used.insert(tmp).second) {
+            int n = word_node.at(tmp);
+            graph[node].push_back(tools::mkDefNode(n));
+            backtrack(word_node, tmp, graph, used);
+          }
+        }
+      }
+    }
     void submit(std::optional<char*> file, bool debug_mode)
     {
         if (file.has_value())
@@ -42,9 +86,26 @@ namespace algorithms::onlinejudge::graph::battleships
             throw std::ios_base::failure(errorMessage);
           }
         
-        while_read(t_case);
-        while(t_cases--) {
-          while_read(S);
-        }  
+        std::map<std::string, int> word_node;
+        std::string word;
+        int node = 0;
+        while(while_read(word))
+          word_node[word] = node++;
+
+        int V = node;
+        tools::Dfs<> dfs_s = tools::init_dfs<>(V);
+        tools::vv_def_node words(V);
+        std::set<std::string> used;
+        for(const auto& p : word_node)
+          if(used.insert(p.first).second)
+            backtrack(word_node, p.first, words, used);
+ 
+        auto top_res = top_sort_kahn(V, words);
+        tools::vi dp(V, 1);
+        for(int u : top_res.top_sort)
+          for (auto& v : words[u])
+            dp[v.node] = std::max(dp[v.node], dp[u] + 1);
+        int max_ladder = *std::max_element(dp.begin(), dp.end());
+        std::cout << max_ladder << std::endl;
     }
 }

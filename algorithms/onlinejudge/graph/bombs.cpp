7a0b844827eba087f6f80888dd4d368538465d5e
Sergey Yakovlev <fclaw007@gmail.com>
2025-06-25 16:40:10 +0300

    online judge: https://onlinejudge.org/external/106/10653.pdf, 10653 Bombs! NO they are Mines!!, ad-hoc soluton rt:0.280s , bfs framework catches TLE

--- algorithms/onlinejudge/graph/bombs.cpp
+++ algorithms/onlinejudge/graph/bombs.cpp
@@ -19,21 +19,17 @@
 #include <bitset>
 #include <unordered_map>
 #include <unordered_set>
+#include <queue>
 
 
-namespace tools = algorithms::onlinejudge::graph::tools;
+// For 4-directional movement (up, right, down, left)
+const std::vector<std::pair<int, int>> dirs_4 = { {-1, 0}, {0, 1}, {1, 0}, {0, -1} };
 
+constexpr int MAXN = 1000;
 
-enum Field { Mine, Free };
-
-std::ostream& operator << (std::ostream& os, const Field& field) 
-{
-    std::string s;
-    if(field == Mine) s = "mine";
-    else if(field == Free) s = "free";
-    return os << s; 
-}
-
+bool visited[MAXN][MAXN];
+int dist[MAXN][MAXN];
+bool mines[MAXN][MAXN];
 
 namespace algorithms::onlinejudge::graph::bombs
 {
@@ -54,41 +50,53 @@ namespace algorithms::onlinejudge::graph::bombs
         
 
         while(while_read(R, C) && R && C) {
+
+          memset(visited, false, sizeof(visited));
+          memset(mines, false, sizeof(mines));
           while_read(B);
-          std::vector<tools::vb> field(R, tools::vb(C, false));
           loop(B, [&](int _) {
             int r, c, n;
             while_read(r, n);
             loop(n, [&](int _) {
               while_read(c);
-              field[r][c] = true;
+              mines[r][c] = true;
             });
           });
-        
-          tools::GridGraph<Field, bool> graph = tools::init_grid_graph<Field, bool>();
-          graph.cell_to_value = [](bool _) -> Field { return Free; };
-          graph.is_blocked = [](Field field) { return field == Mine; };
-          tools::grid_to_adj_list(graph, field, tools::dirs_4);
 
-          int r, c;
-          while_read(r, c);
-          int start_node = r * C + c;
-          tools::Node<Field> source = {start_node, Free};
-          while_read(r, c);
-          int end_node = r * C + c;
-          tools::Node<Field> sink = {end_node, Free};  
 
-          tools::Bfs<Field> bfs_s(R * C, source);
-          bfs_s.check = 
-            [&](const tools::Node<Field>& node)
-              -> tools::BfsCheck { 
-              if(node == sink) 
-                return tools::IsFinished;
-              return tools::GoOn;
-            };
+          int source_r, source_c;
+          while_read(source_r, source_c);
+          int sink_r, sink_c;
+          while_read(sink_r, sink_c);
 
-          tools::bfs(graph.adj, bfs_s);
-          std::cout << bfs_s.getDistance(sink) << std::endl;
+          std::queue<std::pair<int, int>> q;
+          memset(dist, -1, sizeof(dist));
+          dist[source_r][source_c] = 0;
+          q.push({source_r, source_c});
+          visited[source_r][source_c] = true;
+          while(!q.empty()) {
+            auto p = q.front(); q.pop();
+            int r = p.first;
+            int c = p.second;
+            if(mines[r][c]) continue;
+            if(r == sink_r && c == sink_c) break;
+            for(auto d : dirs_4) {
+              int dr = d.first;
+              int dc = d.second;
+              int nr = r + dr, nc = c + dc;
+              auto in_bounds = [&](int r, int c) -> bool {
+                 return r >= 0 && r < R && c >= 0 && c < C; 
+                };
+              if(in_bounds(nr, nc) && 
+                 !visited[nr][nc] && 
+                 !(~dist[nr][nc])) {
+                dist[nr][nc] = dist[r][c] + 1;
+                visited[nr][nc] = true;
+                q.push({nr, nc});
+              }
+            }
+          }
+          std::cout << dist[sink_r][sink_c] << std::endl;  
         }
     }
 }

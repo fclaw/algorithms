e4e32487cd2cd4172b6fc54f5c01a29d188c882b
Sergey Yakovlev <fclaw007@gmail.com>
2025-07-14 15:30:13 +0300

    online judge: https://onlinejudge.org/external/12/1247.pdf, 1247 Interstar Transport

---
+++ algorithms/onlinejudge/graph/interstarTransport.cpp
@@ -0,0 +1,96 @@
+/*
+───────────────────────────────────────────────────────────────
+🧳 UVa 1247 Interstar Transport, rt: s
+───────────────────────────────────────────────────────────────
+*/
+
+#include "../debug.h"
+#include "../../aux.h"
+#include "tools/w_graph.cpp"
+#include "tools/floyd_warshall.cpp"
+
+#include <optional>
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <stdexcept>
+#include <cstring>
+#include <bitset>
+#include <unordered_map>
+#include <unordered_set>
+
+
+
+namespace fd = algorithms::onlinejudge::graph::tools::floyd_warshall;
+namespace wg = algorithms::onlinejudge::graph::tools::wg;
+
+constexpr int inf = 1e5;
+
+struct Route 
+{
+    int cost;
+    int hopes;
+    bool operator < (const Route& other) const { return cost < other.cost || (cost == other.cost && hopes < other.hopes); }
+    bool operator == (const Route& other) const { return cost == other.cost && hopes == other.hopes; }
+    bool operator != (const Route& other) const { return !(*this == other); }
+};
+
+
+namespace algorithms::onlinejudge::graph::interstar_transport
+{
+    /** https://onlinejudge.org/external/12/1247.pdf */
+    int V, E, Q;
+    int getIdx(char c) { return c - '0' - 17; }
+    void submit(std::optional<char*> file, bool debug_mode)
+    {
+        if (file.has_value())
+          // Attempt to reopen stdin with the provided file
+          if (std::freopen(file.value(), "r", stdin) == nullptr) {
+            // If freopen fails, throw an exception with a more detailed error message
+            std::string name = file.value();
+            std::string errorMessage = 
+              "Failed to open file: " + name +
+              " with error: " + std::strerror(errno);
+            throw std::ios_base::failure(errorMessage);
+          }
+        
+        while(while_read(V, E)) {
+          std::vector<char> planets(V);
+          wg::WGraph<Route> graph(V);
+          for(int e = 0; e < E; ++e) {
+            char s_u, s_v;
+            int cost;
+            while_read(s_u, s_v, cost);
+            int u = getIdx(s_u);
+            int v = getIdx(s_v);
+            planets[u] = s_u;
+            planets[v] = s_v;
+            wg::pair(graph, u, v, {cost, 1});
+          }
+
+          fd::FloydWarshall<Route> fw_s(V, {0, 0}, {inf, inf});
+          fw_s.mappend = 
+            [](const Route& u, const Route& v) 
+            { return (Route){u.cost + v.cost, u.hopes + v.hopes}; };
+          fd::floyd_warshall(graph, fw_s);
+
+          while_read(Q);
+          while(Q--) {
+            char s_start, s_end;
+            while_read(s_start, s_end);
+            int start = getIdx(s_start);
+            int end = getIdx(s_end);
+
+            auto xs = fw_s.getPath(start, end);
+            std::string path;
+            for(int idx : xs) {
+              path += std::string(1, planets[idx]) + " ";
+            }
+            path.pop_back();
+            std::cout << path << std::endl;
+          }
+        }
+    }
+}
--- algorithms/onlinejudge/graph/tools/floyd_warshall.cpp
+++ algorithms/onlinejudge/graph/tools/floyd_warshall.cpp
@@ -13,8 +13,17 @@
 
 namespace wg = algorithms::onlinejudge::graph::tools::wg;
 
+template<typename W = int>
+using v_w = std::vector<W>;
+template<typename W = int>
+using vv_w = std::vector<v_w<W>>;
+
 using vi = std::vector<int>;
 using vvi = std::vector<vi>;
+using ll = long long;
+using vl = std::vector<ll>;
+using vvl = std::vector<vl>;
+
 
 constexpr int MAX_V = 450; // if |V| > 450, you cannot use Floyd Washall's
 
@@ -24,18 +33,24 @@ namespace algorithms::onlinejudge::graph::tools::floyd_warshall
     template<typename W = int, typename T = tools::Unit>
     struct FloydWarshall
     {
-        vvi matrix;
+        vv_w<W> matrix;
         vvi path;
         W inf;
         W def;
+        // This will store the NUMBER of distinct shortest paths from u to v.
+        // Use a 64-bit integer to be safe from overflow.
+        vvl path_count; 
         std::function<W(W, W)> mappend;
         FloydWarshall(int V, W def, W inf) {
           if(V > MAX_V) throw std::runtime_error("floyd warshall: cannot support the size greater then 450!");
           this->inf = inf;
           this->def = def;
-          matrix.assign(V, vi(V, inf));
-          for(int v = 0; v < V; ++v) 
-            matrix[v][v] = def; // base neutral element in W (for int is 0)
+          matrix.resize(V, v_w<W>(V));
+          for(int i = 0; i < V; ++i)
+            for(int j = 0; j < V; ++j) {
+              if(i == j) matrix[i][j] = def;
+              else matrix[i][j] = inf;
+          }
         }
 
         vi getPath(int start, int end) const {
@@ -72,7 +87,9 @@ namespace algorithms::onlinejudge::graph::tools::floyd_warshall
       int V = (int)graph.size();
       auto& matrix = fw_s.matrix;
       auto& path = fw_s.path;
+      auto& path_count = fw_s.path_count; 
       path.assign(V, vi(V));
+      path_count.assign(V, vl(V));
 
       for(int i = 0; i < V; ++i) {
         for(int j = 0; j < V; ++j) {
@@ -83,12 +100,18 @@ namespace algorithms::onlinejudge::graph::tools::floyd_warshall
           }
         }
       }
-
+      
       for(int u = 0; u < V; ++u)
         for(const wg::WNode<W, T>& n : graph[u]) {
-          matrix[u][n.node] = std::min(matrix[u][n.node], n.weight);
-          // The next node on the path from u to n.node is n.node itself
-          path[u][n.node] = n.node;
+          int v = n.node;
+          W w = n.weight;  
+          if(w < matrix[u][v]) {
+            matrix[u][v] = w;
+            path_count[u][v] = 1; // a new shortest path
+            path[u][v] = v;
+          } else if (w == matrix[u][v]) {
+            path_count[u][v]++; // Another path of the same shortest length
+          }
         }
 
       for(int k = 0; k < V; ++k) {               // loop order is k->u->v, first fix the intermediate vertex
@@ -98,12 +121,18 @@ namespace algorithms::onlinejudge::graph::tools::floyd_warshall
             if(matrix[u][k] != fw_s.inf && 
                matrix[k][v] != fw_s.inf) {
               W dist = fw_s.mappend(matrix[u][k], matrix[k][v]);
+              ll combined_path_count = path_count[u][k] * path_count[k][v];
               if(dist < matrix[u][v]) {
                 matrix[u][v] = dist;
+                // All old paths are invalid. The new count is paths(u,k) * paths(k,v)
+                path_count[u][v] = combined_path_count;
                 // The path from u to v now goes through k.
                 // So, the first step from u should be the same
                 // as the first step on the path from u to k.
                 path[u][v] = path[u][k];
+              }
+              else if(dist == matrix[u][v]) {
+                path_count[u][v] += combined_path_count;
               }
             }
           }

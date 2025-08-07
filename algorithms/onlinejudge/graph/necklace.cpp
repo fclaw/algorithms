79f53cf504c8b35a7f8f7d5f44885e1d8c5bca1d
Sergey Yakovlev <fclaw007@gmail.com>
2025-08-07 15:04:31 +0300

    online judge: https://onlinejudge.org/external/100/10054.pdf, 10054 The Necklace, rt: 0.380

--- algorithms/onlinejudge/graph/necklace.cpp
+++ algorithms/onlinejudge/graph/necklace.cpp
@@ -14,31 +14,62 @@
 namespace eulerian = algorithms::onlinejudge::graph::tools::eulerian;
 
 
+constexpr int MAX_COLOUR = 50;
+
 struct Bead
 {
-    int index;
-    int origin_index;
     int left_clr;
     int right_clr;
 };
 
-
-bool can_chain(const Bead& lhs, const Bead& rhs) { 
-  return lhs.right_clr == rhs.left_clr && (lhs.origin_index != rhs.origin_index);
-}
-Bead swap(const Bead& bead) { 
-  Bead swapped = bead;
-  swapped.left_clr = bead.right_clr;
-  swapped.right_clr = bead.left_clr;
-  swapped.origin_index = bead.index;
-  return swapped;
-}
-
-
 using ii = std::pair<int, int>;
 using vi = std::vector<int>;
 using vvi = std::vector<vi>;
-using v_bead = std::unordered_map<int, Bead>;
+using v_bead = std::vector<Bead>;
+
+
+bool is_connected(int start, const vvi& adj) {
+    
+    // --- Step 1: Count the total number of "active" vertices ---
+    // An active vertex is one that is part of at least one bead (edge).
+    int total_active_nodes = 0;
+    for (int i = 0; i < MAX_COLOUR; ++i) {
+        if (!adj[i].empty()) {
+            total_active_nodes++;
+        }
+    }
+    
+    // If there are 0 or 1 active nodes, the graph is trivially connected.
+    if (total_active_nodes <= 1) {
+        return true;
+    }
+
+    // --- Step 2: Run a single traversal (BFS is common) to see how many nodes we can reach ---
+    std::vector<bool> visited(MAX_COLOUR, false);
+    std::queue<int> q;
+    int nodes_reached = 0;
+
+    q.push(start);
+    visited[start] = true;
+    
+    while (!q.empty()) {
+        int u = q.front();
+        q.pop();
+        nodes_reached++;
+
+        for (int v : adj[u]) {
+            if (!visited[v]) {
+                visited[v] = true;
+                q.push(v);
+            }
+        }
+    }
+
+    // --- Step 3: Compare the counts ---
+    // If the number of nodes reached by our traversal equals the total number
+    // of active nodes, then the graph is connected.
+    return nodes_reached == total_active_nodes;
+}
 
 
 namespace algorithms::onlinejudge::graph::necklace
@@ -62,34 +93,55 @@ namespace algorithms::onlinejudge::graph::necklace
         while(t_cases--) {
           while_read(beads_n);
           v_bead beads;
-          int ext = 0;
-          for(int bead_idx = 0; bead_idx < beads_n; ++bead_idx) {
+          for(int bead_id = 0; bead_id < beads_n; ++bead_id) {
             Bead bead;
             while_read(bead.left_clr);
             while_read(bead.right_clr);
-            bead.index = bead_idx;
-            bead.origin_index = bead_idx;
-            beads[bead_idx] = bead;
-            if(bead.left_clr != bead.right_clr) {
-              Bead swapped = swap(bead);
-              swapped.index = beads_n + ext++;
-              beads[swapped.index] = swapped;
-            }
+            bead.left_clr--;
+            bead.right_clr--;
+            beads.push_back(bead);
           }
- 
-          int ext_beads_n = beads_n + ext;
-          vvi necklace(beads_n);
-          for(int bead_i = 0; bead_i < beads_n; ++bead_i) {
-            for(int bead_j = 0; bead_j < ext_beads_n; ++bead_j) {
-              if(bead_i != bead_j) {
-                if(can_chain(beads[bead_i], beads[bead_j])) {
-                  int bead_i_idx = beads[bead_i].index;
-                  int bead_j_idx = beads[bead_j].index;
-                  necklace[bead_i_idx].push_back(bead_j_idx);
-                }
-              }
-            }
+
+          vvi colours(MAX_COLOUR);
+          vi degree(MAX_COLOUR);
+          for(int bead_id = 0; bead_id < beads_n; ++bead_id) {
+            int left_clr = beads[bead_id].left_clr;
+            int right_clr = beads[bead_id].right_clr;
+            degree[left_clr]++;
+            degree[right_clr]++;
+            colours[left_clr].push_back(right_clr);
+            colours[right_clr].push_back(left_clr);
           }
+          
+          bool is_even_degree = true;
+          int start = -1;
+          for(int clr = 0; clr < MAX_COLOUR; ++clr) {
+            if((degree[clr] % 2)) {
+              is_even_degree = false;
+              break;
+            }
+            if(start == -1 &&
+               degree[clr] > 0 && 
+               !(degree[clr] % 2)) 
+              start = clr;
+          }
+
+          bool is_necklace = is_even_degree;
+          is_necklace &= (start != -1 && is_connected(start, colours));
+
+          if(is_necklace) {
+            vi path = eulerian::hierholzer_u(start, colours);
+            std::string path_s;
+            for(int i = 1; i < (int)path.size(); ++i) {
+              int left_clr = path[i - 1] + 1;
+              int right_clr = path[i] + 1;
+              path_s += std::to_string(left_clr) + " " + std::to_string(right_clr) + "\n";
+            }
+            printf("Case #%d\n%s", t_case++, path_s.c_str());
+          } else {
+            printf("Case #%d\nsome beads may be lost\n", t_case++);
+          }
+          if(t_cases) printf("\n");
         }
     }
 }
--- algorithms/onlinejudge/graph/tools/eulerian.cpp
+++ algorithms/onlinejudge/graph/tools/eulerian.cpp
@@ -4,21 +4,16 @@ namespace algorithms::onlinejudge::graph::tools::eulerian
 {
     using vi = std::vector<int>;
     using vvi = std::vector<vi>;
-    struct Eulerian {
-      int N;
-      vi path; // This will store the final path
-      Eulerian(int n) : N(n) {}
-    };
 
     // This function assumes the graph has an Eulerian path/circuit
-    void hierholzer(
-      Eulerian& eulerian_s, 
-      vvi& graph, // Pass graph by non-const reference to modify it
-      int start_node) {
+    vi hierholzer_u(
+      int start,
+      // Pass graph by non-const reference to modify it  
+      vvi& graph) {
       
       vi current_path;
-      current_path.push_back(start_node);
-      vi& circuit = eulerian_s.path;
+      current_path.push_back(start);
+      vi circuit;
 
       while (!current_path.empty()) {
         int u = current_path.back();
@@ -49,12 +44,13 @@ namespace algorithms::onlinejudge::graph::tools::eulerian
       }
       // --- The reverse is done ONCE at the end ---
       std::reverse(circuit.begin(), circuit.end());
+      return circuit;
     }
 
 
-    vi hierholzer(int s, const vvi& graph, int N) {
+    vi hierholzer_d(int start, const vvi& graph, int N) {
       vi path, idx(N, 0), st;
-      st.push_back(s);
+      st.push_back(start);
       while(!st.empty()) {
         int u = st.back();
         if (idx[u] < (int)graph[u].size()) {           // still has neighbor

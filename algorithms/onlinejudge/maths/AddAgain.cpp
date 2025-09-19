1a81b0f1fda4c435c623e5a43ce126d1aeccb5a7
Sergey Yakovlev <fclaw007@gmail.com>
2025-09-19 22:32:30 +0300

    11076 Add Again: WA

--- algorithms/onlinejudge/maths/AddAgain.cpp
+++ algorithms/onlinejudge/maths/AddAgain.cpp
@@ -10,22 +10,99 @@
 #include "../debug.h"
 #include "../../aux.h"
 #include "utility/arithmetics.cpp"
+#include "utility/big_integer.cpp"
 #include <bits/stdc++.h>
 
 
 namespace arith = algorithms::onlinejudge::maths::utility::arithmetics;
+namespace bi = algorithms::onlinejudge::maths::utility::big_integer;
 
 
-constexpr int MAX_FAC = 11;
-using ll = long long;
-using vi = std::vector<int>;
+using ull = unsigned long long;
+using vi = std::vector<ull>;
+using map_ll = std::unordered_map<ull, ull>;
+
+constexpr ull MAX_FAC = 12;
 
 
-ll do_factorial(ll n) {
-  ll fac = 1;
-  for(ll i = 1; i <= n; ++i)
+ull do_factorial(ull n) {
+  ull fac = 1;
+  for(ull i = 1; i <= n; ++i)
     fac *= i;
   return fac;  
+}
+
+struct Summation_Unit
+{
+    int lead;
+    map_ll els; // element with its freq
+};
+
+
+using v_sum_unit = std::vector<Summation_Unit>;
+
+v_sum_unit split_into_summation_units(const vi& nums) {
+  v_sum_unit units;
+  std::unordered_set<int> used;
+  for(int n : nums) {
+    if(used.count(n)) continue;
+    used.insert(n);
+    vi tmp = nums;
+    auto it = std::find(tmp.begin(), tmp.end(), n);
+    tmp.erase(it);
+    map_ll els;
+    for(int el : tmp) els[el]++;
+    units.push_back({n, els});
+  }
+  return units;
+}
+
+
+ull do_summation(int s, const Summation_Unit& unit, const std::array<ull, 13>& factorials) {
+  int lead = unit.lead;
+  map_ll els = unit.els;
+  int s_tail = s - 1;
+  // calculate the depth of factorial for a lead in unit
+  int depth = factorials[s_tail];
+  for(auto& p : els) 
+    depth /= factorials[p.second];
+  
+  if(depth == 1 || els.empty()) { // in the form of lead x x x ..
+    std::string sum_s = std::to_string(lead); 
+    for(auto& p : els)
+      for(int i = 1; i <= (int)p.second; ++i)
+        sum_s += std::to_string(p.first);    
+    return std::atoll(sum_s.c_str()); 
+  }
+
+  // multiplication factor: depth / count of elements
+  int cnt = 0;
+  for(auto& p : els) cnt += p.second;
+  int multiplication_factor = depth / cnt;
+
+  ull els_sum = 0;
+  for(auto& p : els) 
+    els_sum += p.first * p.second;
+  els_sum *= multiplication_factor;
+
+  // special case 3 [1 1 2 2], where 4 elements and the depth is 6
+  if((depth % cnt)) {
+    for(auto& p : els) {
+      ull factor = p.second / (depth % cnt);
+      els_sum += p.first * factor;
+    }   
+  }
+
+  ull sum = lead * depth * arith::power<ull>((ull)10, (ull)s_tail);
+  ull exp = 0, carry = 0, curr_el = els_sum;
+  while(exp < (ull)s_tail) {
+    ull rem = curr_el % 10;
+    carry = curr_el / 10;
+    sum += arith::power<ull>((ull)10, exp++) * rem;
+    curr_el = els_sum + carry;
+  }
+  sum += arith::power<ull>((ull)10, exp) * carry;
+  return sum;
 }
 
 
@@ -45,49 +122,22 @@ namespace algorithms::onlinejudge::maths::add_again
           }
         }
 
-        std::array<ll, 12> factorials;
+        std::array<ull, 13> factorials;
         factorials[0] = 0;
-        for(int f = 1; f <= MAX_FAC; ++f)
+        for(ull f = 1; f <= MAX_FAC; ++f)
           factorials[f] = do_factorial(f);
 
         int size;
         while(scanf("%d", &size) == 1 && size) {
           vi nums(size);
-          for(int i = 0; i < size; ++i) 
-            scanf("%d", &nums[i]);
-          
-          if(size == 1) {
-             printf("%d\n", nums.front());
-             continue;
-          }  
-
-          ll factor = factorials[size - 1] / (size -  1);
-          
-          std::unordered_map<int, ll> products;
           for(int i = 0; i < size; ++i)
-            for(int j = 0; j < size; ++j)
-              if(auto it = products.find(nums[i]); i != j) {  
-                if(it == products.end())
-                  products[nums[i]] = nums[j];  
-                else products[nums[i]] += nums[j];
-              }
-  
-          for(auto& p : products) p.second *= factor;
- 
-          ll sum = 0;
-          for(auto& p : products) {
-            ll carry = 0, exp = 0, y = 0, rem = 0;
-            while(exp < size - 1) {  
-              rem = (p.second + carry) % 10;
-              carry = (p.second + carry) / 10;
-              y += arith::power((ll)10, exp) * rem;
-              exp++;
-            }
-            ll x = (p.first * factorials[size - 1] + carry);
-            ll x_digits = arith::power((ll)10, (ll)size - 1);
-            sum += x * x_digits + y;
-          }
-          printf("%lld\n", sum);
+            scanf("%llu", &nums[i]);
+         
+          ull sum = 0;
+          v_sum_unit units = split_into_summation_units(nums);
+          for(auto& unit : units)
+            sum += do_summation(size, unit, factorials);
+          printf("%llu\n", sum);
         }
     }
 }
--- algorithms/onlinejudge/maths/utility/big_integer.cpp
+++ algorithms/onlinejudge/maths/utility/big_integer.cpp
@@ -88,6 +88,32 @@ namespace algorithms::onlinejudge::maths::utility::big_integer
             return result;
         }
 
+        BigInt operator += (const BigInt& other) {
+      int carry = 0;
+    
+    // The loop needs to run as long as there are digits in either number
+    // or there is a carry left over.
+    for (size_t i = 0; i < other.digits.size() || carry > 0; ++i) {
+        // If the current number (*this) runs out of digits, we need to
+        // extend it with zeros to continue the addition.
+        if (i >= this->digits.size()) {
+            this->digits.push_back(0);
+        }
+
+        // Get the i-th digit of the 'other' number, or 0 if it's shorter.
+        int other_digit = (i < other.digits.size()) ? other.digits[i] : 0;
+
+        // Perform the grade-school addition for this "column".
+        int current = this->digits[i] + other_digit + carry;
+        this->digits[i] = current % 10; // The new digit for this position
+        carry = current / 10;           // The new carry for the next position
+    }
+    
+    // Return a reference to the modified object.
+    return *this;
+        }
+        
+
         // You can add more operators like +, -, /, % as needed
         // ...
 
@@ -97,6 +123,26 @@ namespace algorithms::onlinejudge::maths::utility::big_integer
         const std::vector<int>& get_digits() const {
             return digits;
         }
+
+        std::string get_string() const {
+          std::string s;
+          for(auto d : digits)
+            s += std::to_string(d);
+          return s;
+        }
     };
 
+    BigInt operator + (const BigInt& lhs, const BigInt& rhs) {
+         // Determine which number has more digits to use as the base for our sum.
+    // This is a small optimization to reduce vector reallocations.
+    if (lhs.get_digits().size() > rhs.get_digits().size()) {
+        BigInt result = lhs; // Make a copy of the larger number
+        result += rhs;      // Add the smaller one to it
+        return result;
+    } else {
+        BigInt result = rhs; // Make a copy of the larger (or equal-sized) number
+        result += lhs;      // Add the smaller one to it
+        return result;
+    }
+    }
 }

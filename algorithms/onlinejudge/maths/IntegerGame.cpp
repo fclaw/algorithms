40631b1e707c1f4e1e39befc1c17556b3c81892e
Sergey Yakovlev <fclaw007@gmail.com>
2025-10-27 10:31:04 +0300

    onlinejudge: 11489 Integer Game, commentry

--- algorithms/onlinejudge/maths/IntegerGame.cpp
+++ algorithms/onlinejudge/maths/IntegerGame.cpp
@@ -42,22 +42,64 @@ bool is_win(int sum, std::vector<int>& freq) {
 
   for(int take = 1; take <= 9; ++take) {
 
-    if((take == 3 || take == 6 || take == 9) && freq[take] > 0) {
+    // --- Strategic Shortcut: The "Pass the Turn" Optimization ---
+    //
+    // The core insight for making this solution efficient is to handle digits divisible
+    // by 3 ({3, 6, 9}) as a special case.
+    //
+    // WHY IS IT SPECIAL?
+    // When a player removes a digit 'd' where (d % 3 == 0), the sum of the remaining
+    // digits modulo 3 DOES NOT CHANGE. (S - d) % 3 == S % 3.
+    // This means if the opponent was not in a position to make an immediate winning
+    // move before, they are STILL not in a position to win. Such a move doesn't
+    // change the strategic landscape of the game; it effectively just "passes the turn."
+    //
+    // THE OPTIMIZATION:
+    // Instead of recursively simulating the removal of these "passing" moves one by one,
+    // which would be very slow if there are many of them, we can determine the winner of
+    // this "mini-game" of turn-passing with a single parity argument.
+    //
+    // We can "fast-forward" by imagining all 'k' = freq[take] of these digits are the
+    // only moves left to play. Players will take them one by one.
+    //   - If 'k' is ODD: The first player (us) will take the 1st, 3rd,..., last one.
+    //     This leaves the OPPONENT to face the remaining subgame.
+    //   - If 'k' is EVEN: The first player (us) will take the 1st, 3rd,..., (k-1)th one.
+    //     The opponent takes the last one, leaving US to face the remaining subgame.
+    //
+    // THE LOGIC:
+    // Let `res = is_win(subgame)` be the result of the game *after* all these digits are gone.
+    //   - If freq[take] is ODD: We want the opponent to lose. So, we win if `!res` is true.
+    //   - If freq[take] is EVEN: We want ourselves to lose the subgame (since the opponent wins it). No, we want ourselves to win.
+    //     If `freq[take]` is even, we are the one left to face the subgame. So we win if `res` is true.
+    //
+    // This block implements this parity logic, effectively pruning a long, linear chain
+    // from the game tree and replacing it with an O(1) calculation. This is critical for
+    // passing the time limit on large inputs.
+
+    if((take == 3 || 
+        take == 6 || 
+        take == 9) && 
+        freq[take] > 0) {
       int new_sum = sum - take * freq[take];
       if(new_sum >= 0 &&
          new_sum % 3 == 0) {
         int old_freq = freq[take];
         freq[take] = 0;
-        bool res = is_win(new_sum, freq);
+        bool opponent_wins_sub_game = is_win(new_sum, freq);
         freq[take] = old_freq;
-        if(freq[take] % 2 != 0 && !res)
-          return memo[{sum, freq}] = true;
-        else if(old_freq % 2 == 0 && !res)
-          return memo[{sum, freq}] = false;
-        else if (old_freq % 2 != 0 && res)
-          return memo[{sum, freq}] = false;
-        else if (old_freq % 2 == 0 && res)
-          return memo[{sum, freq}] = true;
+       
+        // Apply the parity logic
+        if(old_freq % 2 != 0) { // ODD number of these moves. Opponent faces the sub-game.
+          if(!opponent_wins_sub_game) {
+            return memo[{sum, freq}] = true; // We can force a win.
+          }
+        } else { // EVEN number of these moves. We face the sub-game.
+          if(opponent_wins_sub_game) { // It's our turn, and we know it's a winning sub-game for the current player.
+            return memo[{sum, freq}] = true; // We can force a win.
+          }
+        }
+        // This path was not a guaranteed win, so we continue the main loop to check other moves.
+        continue; // A 'continue' makes the intent clear.
       }
     }
 

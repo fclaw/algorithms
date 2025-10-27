8e081d7ffdaf22b15ed14f827e7b40616c9e6fc8
Sergey Yakovlev <fclaw007@gmail.com>
2025-10-27 13:54:20 +0300

    onlinejudge: 11311 Exclusively Edible

--- algorithms/onlinejudge/maths/ExclusivelyEdible.cpp
+++ algorithms/onlinejudge/maths/ExclusivelyEdible.cpp
@@ -1,6 +1,10 @@
 /*
 ───────────────────────────────────────────────────────────────
 🧳 UVa 11311 Exclusively Edible, https://onlinejudge.org/external/113/11311.pdf, rt: s
+   game theory, reducible to Nim game; 
+   we can view the game that Hansel and Gretel are playing as Nim game, 
+   where there are 4 heaps - cakes left/below/right/above the topping; 
+   take the Nim sum of these 4 values and if they are equal to 0, Gretel loses)
 ───────────────────────────────────────────────────────────────
 */
 
@@ -25,6 +29,13 @@ namespace algorithms::onlinejudge::maths::exclusively_edible
               " with error: " + std::strerror(errno);
             throw std::ios_base::failure(errorMessage);
           }
+        }
+
+        int t_cases, m, n, r, c;
+        scanf("%d", &t_cases);
+        while(t_cases--) {
+          scanf("%d %d %d %d", &m, &n, &r, &c);
+          printf("%s\n", r ^ c ^ (m - r - 1) ^ (n - c - 1) ? "Gretel" : "Hansel");
         }
     }
 }

d3fcd8fc1d88ccf1887d617577c0411789fdeb9b
Sergey Yakovlev <fclaw007@gmail.com>
2025-10-29 13:05:18 +0300

    onlinejudge: 11195 Another n-Queen Problem, minor fixes

--- algorithms/onlinejudge/advanced_topics/NQueens.cpp
+++ algorithms/onlinejudge/advanced_topics/NQueens.cpp
@@ -15,17 +15,17 @@ typedef long long ll;
 
 int n, mask[32];
 
-ll bf(int row, int col, int left_diagonal, int right_diagonal) {
-  if (row == n) return 1; // done, 1 way found
+ll solve(int row, int col, int left_diagonal, int right_diagonal) {
+  if(row == n) return 1; // done, 1 way found
   // bit = all possible column positions
   int bit = mask[row] | col | left_diagonal | right_diagonal; // list of forbidden column
   bit = ~bit; // after negation, list of allowed columns, but it turns on more bits than necessary
-  bit &= (1<<n) - 1; // (1<<n) - 1, turn on n bits only, we want to concentrate on these n bits only
+  bit &= (1 << n) - 1; // (1<<n) - 1, turn on n bits only, we want to concentrate on these n bits only
   ll total = 0; // now recursively count the ways
   while (bit) { // try each bit that is turned on
     int t = LSOne(bit); // the first bit that is on
     // for future iterations, forbid column t and left_diagonal/right diagonal that has t
-    total += bf(row + 1, col | t, (left_diagonal|t) >> 1, (right_diagonal|t) << 1);
+    total += solve(row + 1, col | t, (left_diagonal | t) >> 1, (right_diagonal | t) << 1);
     bit -= t; // remove that bit t from 'bit'
   }
   return total;
@@ -58,7 +58,7 @@ namespace algorithms::onlinejudge::advanced_topics::n_queens
               if(M[j] == '*')
                 mask[i] |= (1<<j); // mark out the bad squares
           }
-          printf("Case %d: %lld\n", t_case++, bf(0, 0, 0, 0));
+          printf("Case %d: %lld\n", t_case++, solve(0, 0, 0, 0));
         }
     }
 }

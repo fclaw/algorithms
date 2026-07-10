73ff8b84fd61e718397c3c1988a4a92b23f61baf
Sergey Yakovlev <fclaw007@gmail.com>
2026-07-10 15:44:04 +0300

    11734 Big Number of Teams will Solve This

--- algorithms/onlinejudge/strings/Verdict.cpp
+++ algorithms/onlinejudge/strings/Verdict.cpp
@@ -9,6 +9,70 @@
 #include <bits/stdc++.h>
 
 
+enum Verdict { Yes, WrongAnswer, FormatError};
+
+
+
+bool check_format_error(const std::string& input) {
+  bool is_prev_space = false;
+  for(char c : input) {
+    if(std::isspace(c)) {
+      if(!is_prev_space) {
+        return true;
+      } else {
+        is_prev_space = true;
+      }
+    } else {
+      if(is_prev_space) {
+        is_prev_space = false;
+      }
+    }
+  }
+  return false;
+}
+
+bool check_wrong_answer(const std::string& judge, const std::string& input) {
+  
+   std::string no_spaces_input;
+   for(char c : input) {
+     if(!std::isspace(c)) {
+        no_spaces_input.push_back(c);
+     }
+   }
+
+  if(no_spaces_input.size() > judge.size()) {
+    return true;
+  }
+
+  for(int i = 0; i < (int)judge.size(); ++i) {
+    if(judge[i] != no_spaces_input[i]) {
+      return true;
+    }
+  }
+  return false;
+}
+
+
+Verdict get_verdict(const std::string& judge, const std::string& contestant) {
+  if(check_wrong_answer(judge, contestant)) {
+    return WrongAnswer; 
+  } else if(check_format_error(contestant)) {
+    return FormatError;
+  } else {
+    return Yes;
+  }
+}
+
+std::string verdict_to_string(Verdict verdict) {
+    switch(verdict) {
+      case Yes:
+        return "Yes";
+      case WrongAnswer:
+        return "Wrong Answer";
+      default:
+        return "Output Format Error";
+    }
+}
 
 namespace algorithms::onlinejudge::strings::verdict
 {
@@ -25,6 +89,17 @@ namespace algorithms::onlinejudge::strings::verdict
               " with error: " + std::strerror(errno);
             throw std::ios_base::failure(errorMessage);
           }
+        }
+
+        int t_cases, t_case = 1;
+        std::cin >> t_cases;
+        std::cin.ignore();
+        while(t_cases--) {
+          std::string contestant;
+          std::string judge;
+          std::getline(std::cin, contestant);
+          std::getline(std::cin, judge);
+          printf("Case %d: %s\n", t_case++, verdict_to_string(get_verdict(judge, contestant)).c_str());
         }
     }
 }

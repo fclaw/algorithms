693b6469f56593a6c0a7d0dbb8f275fa8e1a3547
Sergey Yakovlev <fclaw007@gmail.com>
2026-07-19 22:07:51 +0300

    662 Fast Food: path reconstuction

--- algorithms/onlinejudge/dp/FastFood.cpp
+++ algorithms/onlinejudge/dp/FastFood.cpp
@@ -29,7 +29,6 @@ using v_depots = std::vector<Depot>;
 struct Ans
 {
     int dist;
-    v_depots depots;
     bool operator < (const Ans& other) const {
       return dist < other.dist;
     }
@@ -37,7 +36,6 @@ struct Ans
      // In-place addition
     Ans& operator += (const Ans& other) {
       this->dist += other.dist;
-      this->depots.insert(this->depots.end(), other.depots.begin(), other.depots.end());
       return *this;
     }
 
@@ -53,16 +51,15 @@ Ans cache[201][201][31];
 Ans minimize_sum(const vi& restaurants, int left, int right, int depots_left) {
   
    if(left >= right) {
-     return (cache[left][right][depots_left] = {depots_left == 0 ? 0 : MAX, {}});
+     return (cache[left][right][depots_left] = {depots_left == 0 ? 0 : MAX});
    }
 
   if(~cache[left][right][depots_left].dist) {
     return cache[left][right][depots_left];
   }
 
-   Ans ans = {MAX, {}};
+   Ans ans = {MAX};
    for(int p = left; p < right; ++p) {
-     int idx = K - depots_left;
      int d = restaurants[p];
      for(int l = p; l >= left; --l) {
        for(int r = p; r < right; ++r) {
@@ -76,7 +73,7 @@ Ans minimize_sum(const vi& restaurants, int left, int right, int depots_left) {
           for(int i = l; i <= r; ++i) {
            sum += std::abs(restaurants[i] - d);
           }
-         Ans curr_ans = {sum, {{idx, p, l, r}}};
+         Ans curr_ans = {sum};
          for(int d = 0; d < depots_left; ++d) {
           int right_depots = depots_left - d - 1;
           if(l - left > d && right - r - 1 > right_depots) {
@@ -91,6 +88,46 @@ Ans minimize_sum(const vi& restaurants, int left, int right, int depots_left) {
      }
    }
    return (cache[left][right][depots_left] = ans);
+}
+
+void reconstruct_path(const vi& restaurants, int left, int right, int depots_left, v_depots& out_depots) {
+  if (left >= right) {
+    return;
+  }
+
+  int best_dist = cache[left][right][depots_left].dist;
+
+  // Search the choices for the one that produced best_dist
+  for(int p = left; p < right; ++p) {
+    int d = restaurants[p];
+    int idx = K - depots_left;
+    for(int l = p; l >= left; --l) {
+      for(int r = p; r < right; ++r) {
+                
+        int sum = 0;
+        for(int i = l; i <= r; ++i) {
+          sum += std::abs(restaurants[i] - d);
+        }
+
+        for (int d = 0; d < depots_left; ++d) {
+          int right_depots = depots_left - d - 1;
+
+          // Base case lookups
+          int left_val = cache[left][l][d].dist;
+          int right_val = cache[r + 1][right][right_depots].dist;
+
+          if(sum + left_val + right_val == best_dist) {
+            // Found the matching choice!
+            out_depots.push_back({idx, p, l, r});
+            // Recurse to solve left and right segments
+            reconstruct_path(restaurants, left, l, d, out_depots);
+            reconstruct_path(restaurants, r + 1, right, right_depots, out_depots);
+            return; // Successfully reconstructed this branch
+          }
+        }
+      }
+    }
+  }
 }
 
 
@@ -120,21 +157,29 @@ namespace algorithms::onlinejudge::dp::fast_food
           for(int i = 0; i <= 200; ++i) {
             for(int j = 0; j <= 200; ++j) {
               for(int k = 0; k <= 30; ++k) {
-                cache[i][j][k] = {-1, {}};
+                cache[i][j][k] = {-1};
               }
             }
           }
 
           Ans ans = minimize_sum(restaurants, 0, N, K);
+          v_depots depots;
+          reconstruct_path(restaurants, 0, N, K, depots);
           printf("Chain %d\n", t_cases++);
-          for(auto& depot : ans.depots) {
+          for(auto& depot : depots) {
             int idx = depot.idx + 1;
             int r = depot.restaurant_id + 1;
             int from = depot.from + 1;
             int to = depot.to + 1;
-            std::string served = "restaurant " + std::to_string(from);
+            std::string served = 
+              "restaurant " + 
+              std::to_string(from);
             if(from < to) {
-              served = "restaurants " + std::to_string(from) + " to " + std::to_string(to);
+              served = 
+                "restaurants " + 
+                std::to_string(from) + 
+                " to " + 
+                std::to_string(to);
             }
             printf("Depot %d at restaurant %d serves %s\n", idx, r, served.c_str());
           }

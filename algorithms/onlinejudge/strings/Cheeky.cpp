36caa499794c746a5305223226b0ce31c979ee2a
Sergey Yakovlev <fclaw007@gmail.com>
2026-07-24 15:49:33 +0300

    11452 Dancing the Cheeky-Cheeky

--- algorithms/onlinejudge/strings/Cheeky.cpp
+++ algorithms/onlinejudge/strings/Cheeky.cpp
@@ -11,6 +11,43 @@
 
 
 
+std::string find_dance(const std::string& steps) {
+  
+  int S = (int)steps.size();
+  int period = 0;
+  int start = 0;
+  for(int p = 1; p <= S / 2; ++p) {
+    for(int noise = 0; noise <= S - 2 * p; ++noise) {
+      bool is_period = true;
+      int curr_start = 0;
+      for(int i = noise; i < noise + p; ++i) {
+        int next = i + p;
+        if(steps[i] != steps[next]) {
+          is_period = false;
+          curr_start = i;
+          break;
+        }
+      }
+
+      if(is_period) {
+        if(period < p) {
+          start = curr_start;
+        }
+        period = std::max(period, p);
+      }
+    }
+  }
+
+  std::string base = steps.substr(start, period);
+  std::string continuation = base.substr(S - start - 2 * period, base.length());
+  for(int i = 1; i <= 8; ++i) {
+    continuation += base; 
+  }
+  
+  return (continuation.substr(0, 8) + "...");
+}
+
+
 namespace algorithms::onlinejudge::strings::cheeky
 {
 
@@ -31,8 +68,9 @@ namespace algorithms::onlinejudge::strings::cheeky
         int t_cases;
         std::cin >> t_cases;
         while(t_cases--) {
-          std::string steps_seq;
-          std::cin >> steps_seq;
+          std::string steps;
+          std::cin >> steps;
+          std::cout << find_dance(steps) << std::endl;
         }
     }
 }
